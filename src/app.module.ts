c605b13f856f1ad2edd9019f70852c43bff0cf3e
zerozoo <zerozoo385@gmail.com>
2023-09-13 07:46:03 +0900

    add testing http methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
